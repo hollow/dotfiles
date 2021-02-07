567e9c6db87681aa26400d55703f524941e50848
Benedikt Böhm <bb@xnull.de>
2021-02-07 09:57:08 +0100

    add more network tools

--- zsh/net.zsh
+++ zsh/net.zsh
@@ -1,6 +1,43 @@
-# https://github.com/ThomasHabets/arping
-_brew_install arping
+# https://github.com/measurement-factory/dnstop
+# display dns traffic statistics
+_brew_install dnstop
 
 # https://github.com/ogham/dog
 # command-line DNS client
 _brew_install dog
+
+# https://github.com/imapsync/imapsync
+# imap transfer tool
+_brew_install imapsync
+
+# http://jodies.de/ipcalc
+# IP address calculator
+_brew_install ipcalc
+
+# https://github.com/brona/iproute2mac
+# wrapper for MacOS inspired by iproute2
+_brew_install iproute2mac
+
+# https://github.com/traviscross/mtr
+# a network diagnostic tool
+_brew_install mtr
+
+# https://github.com/nmap/nmap
+# the network mapper
+_brew_install nmap
+
+# https://github.com/ntop/ntopng
+# network traffic monitor
+_brew_install ntopng
+
+# https://github.com/denilsonsa/prettyping
+# colorful, compact wrapper for ping
+_brew_install prettyping
+
+# https://github.com/sivel/speedtest-cli
+# test internet bandwidth using speedtest.net
+_brew_install speedtest-cli
+
+# https://github.com/jpoliv/wakeonlan
+# wake up computers via Wake-On-LAN magic packets
+_brew_install wakeonlan
