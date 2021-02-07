7b4b561622bb8cf9bb492edbae1fb741e05d0417
Benedikt Böhm <bb@xnull.de>
2021-02-07 17:12:02 +0100

    add secrets support

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -6,3 +6,21 @@ export GNUPGHOME="${XDG_CONFIG_HOME}"/gnupg
 # load ssh keys into agent
 zinit light-mode lucid for \
     OMZP::gpg-agent
+
+# https://github.com/chuwy/zsh-secrets
+# store gpg encrypted environment variables
+zinit light-mode lucid for \
+    @chuwy/zsh-secrets
+
+export RECEPIENT="${DEFAULT_EMAIL}"
+export SECRETS_STORAGE="${XDG_CONFIG_HOME}/secrets"
+mkdir -p "${SECRETS_STORAGE}"
+
+_has_secret() {
+    if [[ -e "${SECRETS_STORAGE}/${1}.gpg" ]]; then
+        secrets source "${1}" 2>/dev/null || return 1
+        return 0
+    else
+        return 1
+    fi
+}
