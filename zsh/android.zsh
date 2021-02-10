aae2e8bed4631c773d1caafec965e348f58ebf3d
Benedikt Böhm <bb@xnull.de>
2021-02-10 02:26:19 +0100

    update brew bundle

--- zsh/android.zsh
+++ zsh/android.zsh
@@ -12,3 +12,6 @@ fi
 export ANDROID_PREFS_ROOT="${XDG_CONFIG_HOME}"/android
 export ADB_KEYS_PATH="${ANDROID_PREFS_ROOT}"
 export ANDROID_EMULATOR_HOME="${XDG_DATA_HOME}"/android/emulator
+
+# TODO: android sdk
+# TODO: android studio
