53af57fc1a0d73263faf620eb965035f18cd0296
Benedikt Böhm <bb@xnull.de>
2021-02-19 22:57:00 +0100

    install terraspace

--- zsh/terraform.zsh
+++ zsh/terraform.zsh
@@ -1,6 +1,13 @@
 # https://www.terraform.io/
 _brew_install terraform
+alias tf=terraform
 
 # make terraform adhere to XDG
 export TF_CLI_CONFIG_FILE="${XDG_CONFIG_HOME}"/terraform/config
 mkdir -p "${XDG_CACHE_HOME}"/terraform/plugin-cache
+
+# https://terraspace.cloud
+if ! (( $+commands[terraspace] )); then
+    gem install terraspace
+fi
+alias ts=terraspace
