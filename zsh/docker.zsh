17744dbfd47767e9c577efd505b92220b5f39b2e
Benedikt Böhm <bb@xnull.de>
2021-02-19 18:42:07 +0100

    improve docker support

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -4,6 +4,9 @@ if [[ "${OSTYPE}" == darwin* ]]; then
     _cask_install docker
 fi
 
+# make sure docker adheres to XDG
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}"/docker
+
 # https://github.com/bcicen/ctop
 # top-like interface for container metrics
 _brew_install ctop
