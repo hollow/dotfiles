e0e2fe8295902373e9597a694bbeb3907d8e7fe0
Benedikt Böhm <bb@xnull.de>
2021-02-23 13:56:54 +0100

    s/terraspace/terragrunt/

--- zsh/terraform.zsh
+++ zsh/terraform.zsh
@@ -4,10 +4,9 @@ alias tf=terraform
 
 # make terraform adhere to XDG
 export TF_CLI_CONFIG_FILE="${XDG_CONFIG_HOME}"/terraform/config
-mkdir -p "${XDG_CACHE_HOME}"/terraform/plugin-cache
+export TF_PLUGIN_CACHE_DIR="${XDG_CACHE_HOME}"/terraform/plugin-cache
+mkdir -p "${TF_PLUGIN_CACHE_DIR}"
 
-# https://terraspace.cloud
-if ! (( $+commands[terraspace] )); then
-    gem install terraspace
-fi
-alias ts=terraspace
+# https://terragrunt.gruntwork.io/
+_brew_install terragrunt
+alias tg=terragrunt
