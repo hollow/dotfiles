cb8f70fd36ad8f3509223463a5e8a8880fd73e90
Benedikt Böhm <bb@xnull.de>
2021-03-11 14:06:40 +0100

    add terragrunt aliases

--- zsh/terraform.zsh
+++ zsh/terraform.zsh
@@ -2,11 +2,19 @@
 _brew_install terraform
 alias tf=terraform
 
+# https://terragrunt.gruntwork.io/
+_brew_install terragrunt
+alias tg=terragrunt
+
+ti() {
+    terragrunt init -reconfigure -upgrade
+}
+
+ta() {
+    terragrunt apply -compact-warnings
+}
+
 # make terraform adhere to XDG
 export TF_CLI_CONFIG_FILE="${XDG_CONFIG_HOME}"/terraform/config
 export TF_PLUGIN_CACHE_DIR="${XDG_CACHE_HOME}"/terraform/plugin-cache
 mkdir -p "${TF_PLUGIN_CACHE_DIR}"
-
-# https://terragrunt.gruntwork.io/
-_brew_install terragrunt
-alias tg=terragrunt
