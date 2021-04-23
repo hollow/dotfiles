219e882c65861abb8ecd01c14fdf89c331214b5d
Benedikt Böhm <bb@xnull.de>
2021-04-23 08:33:20 +0200

    remove terragrunt

--- zsh/terraform.zsh
+++ zsh/terraform.zsh
@@ -2,18 +2,6 @@
 _brew_install terraform
 alias tf=terraform
 
-# https://terragrunt.gruntwork.io/
-_brew_install terragrunt
-alias tg=terragrunt
-
-ti() {
-    terragrunt init -reconfigure -upgrade
-}
-
-ta() {
-    terragrunt apply -compact-warnings
-}
-
 # make terraform adhere to XDG
 ln -nfs "${XDG_CONFIG_HOME}"/terraform/config "${HOME}"/.terraformrc
 ln -nfs "${XDG_CONFIG_HOME}"/terraform "${HOME}"/.terraform.d
