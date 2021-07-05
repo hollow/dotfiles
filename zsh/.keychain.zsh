8f93c4c5384a36ea47ddbcc535cb444eb05a8984
Benedikt Böhm <bb@xnull.de>
2021-07-05 13:47:47 +0000

    do not load gpg agent for now

--- zsh/.keychain.zsh
+++ zsh/.keychain.zsh
@@ -5,7 +5,6 @@ eval $(
         --inherit any \
         --absolute \
         --dir "${XDG_DATA_HOME}/keychain" \
-        --agents ssh,gpg \
-        id_rsa id_ed25519 \
-        ${USER_EMAIL}
+        --agents ssh \
+        id_rsa id_ed25519
 )
