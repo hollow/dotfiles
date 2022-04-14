07dd1a4442d5eb10b2b765e077545b9ba5781379
Benedikt Böhm <bb@xnull.de>
2022-04-14 13:04:28 +0200

    format python

--- python/startup.py
+++ python/startup.py
@@ -5,10 +5,11 @@ import readline
 
 history = os.path.join(os.getenv("XDG_DATA_HOME"), "python/history")
 
-readline.parse_and_bind('tab: complete')
+readline.parse_and_bind("tab: complete")
 
 if os.path.exists(history):
     readline.read_history_file(history)
+
 
 @atexit.register
 def write_history(history=history):
